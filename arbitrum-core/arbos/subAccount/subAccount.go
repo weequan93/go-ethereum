a051a5faa08d13aab55c47400903b552a91dfd12
weequan <wee@hyperlegend.onmicrosoft.com>
2025-06-10 09:40:37 +0800

    enhance check on  usdt approve target

--- arbitrum-core/arbos/subAccount/subAccount.go
+++ arbitrum-core/arbos/subAccount/subAccount.go
@@ -268,7 +268,7 @@ func (subAccountState *SubAccountState) IsAllowedUsdtAddress(contractAddress com
 	isUsdtMember, err := subAccountState.usdtAddress.IsMember(contractAddress)
 
 	if err != nil {
-		return true, err
+		return false, err
 	}
 
 	if isUsdtMember == true {
@@ -277,7 +277,18 @@ func (subAccountState *SubAccountState) IsAllowedUsdtAddress(contractAddress com
 		}
 		// is usdt address, cmp op
 		if bytes.Equal(txData[:4], ERC20_APPROVE_OP_SIG) {
-			return true, nil
+			// is subaccount allowed address
+			spender := common.BytesToAddress(txData[16:36])
+			isAllowedAddress, err := subAccountState.AllowedAddress().IsMember(spender)
+			if err != nil {
+				return false, err
+			}
+
+			if isAllowedAddress == true {
+				return true, nil
+			}
+
+			return false, nil
 		} else {
 			return false, nil
 		}
